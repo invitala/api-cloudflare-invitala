c69253f7e14db48f4cbebe14f2720c99e359d4d6
Victor Lara <vicblack15@hotmail.com>
2024-08-08 17:09:34 -0600

    add assistant configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/d1';
-import { song } from './db/schema';
+import { song, assistant } from './db/schema';
 import { Hono } from 'hono'
 
 export type Env = {
@@ -41,6 +41,38 @@ api
       );
     }
   })
+  .get('/assistant/', async (c) => {
+    try { 
+      const db = drizzle(c.env.DB);
+      const assistants = await db.select().from(assistant);
+      return c.json(assistants);
+    } catch(error){
+        return c.json(
+          { error,}, 400
+        );
+      }
+    })
+    .post('/assistant/', async (c) => {
+      const body = await c.req.json()
+      const new_assistant = {
+        full_name: body.full_name as string,
+        comment: body.comment as string,
+        is_ceremony: body.is_ceremony as string,
+        is_celebration: body.is_celebration as string
+      } 
+
+      try {
+        const db = drizzle(c.env.DB);
+        await db.insert(assistant).values(new_assistant)
+  
+        return new Response('Created', { status: 201 })
+      } catch (error) {
+        console.log(error)
+        return c.json(
+          {error}, 400
+        );
+      }
+    })
   
 const app = new Hono();
 app.route('/api', api);
